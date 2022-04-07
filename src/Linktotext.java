54d979ff5e49cb5ac8dda5a1f609b1edaaaf12d5
Tanpreetkaur16 <98710669+Tanpreetkaur16@users.noreply.github.com>
2022-04-06 22:42:40 -0400

    Update Linktotext.java

--- src/Linktotext.java
+++ src/Linktotext.java
@@ -12,15 +12,16 @@ import java.util.HashMap;
 public class Linktotext {
 	
 	public static void htmlToTextParser(HashMap<String,String> UT) throws IOException {
+		// differentiating keys and value from filenames and urls from given hashmaps storing it in array of string
 		Object[] key = UT.keySet().toArray();
 		String[] URL = Arrays.stream(key).toArray(String[]::new);
 
 		Object[] fname = UT.values().toArray();
 		String[] fileName = Arrays.stream(fname).toArray(String[]::new);
 
-		
+		//parsing through the loop and storing the filenames as text files
 		for(int s = 0; s<URL.length ; s++) {
-	 
+	            // jsoup always connect to the url and returns to the html file
 		    String document = Jsoup.connect((String) URL[s]).get().html();
 		    Document doc = Jsoup.parse(document);
 		    String HTMLCode = doc.toString();
@@ -33,13 +34,14 @@ public class Linktotext {
 		    HTMLCode = "";
 		    
 		    for(Element element: doc.getAllElements()) {
-	            // Get text from each dom element and write that to the text file
+	            // Get text from each dom element 
 		    	for (Node n : element.childNodes()) {
 		            if (n instanceof TextNode && !((TextNode) n).isBlank()) {
 		            	HTMLCode += ((TextNode) n).text();
 		            } 
 		        }
 	        } 
+			//writing to the text file
 		    writerTxtToFile = new BufferedWriter(new FileWriter("/Users/manpreetsingh/Downloads/searchENGranking/Files/Text/"+fileName[s]+".txt"));
 		    writerTxtToFile.write(HTMLCode);                                                                                 // Write the text to a file
 		    writerTxtToFile.close();
